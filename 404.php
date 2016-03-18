322dfabf6ebe6ee89f2abd055ece101315c90d30
ErikBoesen <me@erikboesen.com>
2016-03-18 08:38:58 -0400

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <title>nothing's here</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+
+<body>
+    <p><a href="<?php bloginfo('url'); ?>">home.</a></p>
+</body>
+</html>
