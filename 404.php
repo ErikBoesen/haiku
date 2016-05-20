7daf7269ddd79fd123aba2a87e8a13226c2f1060
Karlo Sintic <karlo.sintic@gmail.com>
2016-05-20 19:35:49 +0200

    custom error 404 text
    + a few changes with naming things.

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-    <title>404</title>
+    <title><?php echo bloginfo( 'name' )." - 404"; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <style>
@@ -14,11 +14,17 @@
             display: flex;
             justify-content: center;
             align-items: center;
+						flex-direction: column;
+
         }
     </style>
-</head>
 
+<?php
+    $options = get_option( 'haiku_settings' );
+?>
+</head>
 <body>
+    <h1><?php echo $options['txt_404'] ?></h1> </br>
     <p><a href="<?php bloginfo('url'); ?>">home.</a></p>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -3,75 +3,89 @@ add_action( 'admin_menu', 'haiku_add_admin_menu' );
 add_action( 'admin_init', 'haiku_settings_init' );
 
 
-function haiku_add_admin_menu(  ) { 
+function haiku_add_admin_menu(  ) {
 
 	add_menu_page( 'haiku', 'haiku', 'manage_options', 'haiku', 'haiku_options_page' );
 
 }
 
-
-function haiku_settings_init(  ) { 
+function haiku_settings_init(  ) {
 
 	register_setting( 'pluginPage', 'haiku_settings' );
 
 	add_settings_section(
-		'haiku_pluginPage_section', 
-		__( '', 'wordpress' ), 
-		'haiku_settings_section_callback', 
+		'haiku_pluginPage_section',
+		__( '', 'wordpress' ),
+		'haiku_settings_section_callback',
 		'pluginPage'
 	);
 
-	add_settings_field( 
-		'txt', 
-		__( 'before text', 'wordpress' ), 
-		'txt_render', 
-		'pluginPage', 
-		'haiku_pluginPage_section' 
+	add_settings_field(
+		'txt_prev',
+		__( 'previous text', 'wordpress' ),
+		'txt_prev_render',
+		'pluginPage',
+		'haiku_pluginPage_section'
 	);
 
-	add_settings_field( 
-		'haiku_text_field_1', 
-		__( 'after text', 'wordpress' ), 
-		'haiku_text_field_1_render', 
-		'pluginPage', 
-		'haiku_pluginPage_section' 
+	add_settings_field(
+		'txt_next',
+		__( 'next text', 'wordpress' ),
+		'txt_next_render',
+		'pluginPage',
+		'haiku_pluginPage_section'
 	);
 
+	add_settings_field(
+		'txt_404',
+		__( 'error 404 message', 'wordpress' ),
+		'txt_404_render',
+		'pluginPage',
+		'haiku_pluginPage_section'
+	);
 
 }
 
 
-function txt_render(  ) { 
+function txt_prev_render(  ) {
 
 	$options = get_option( 'haiku_settings' );
 	?>
-	<input type='text' name='haiku_settings[txt]' value='<?php echo $options['txt']; ?>'>
+	<input type='text' name='haiku_settings[txt_prev]' value='<?php echo $options['txt_prev']; ?>'>
+	<?php
+
+}
+
+function txt_next_render(  ) {
+
+	$options = get_option( 'haiku_settings' );
+	?>
+	<input type='text' name='haiku_settings[txt_next]' value='<?php echo $options['txt_next']; ?>'>
+	<?php
+
+}
+
+function txt_404_render(  ) {
+
+	$options = get_option( 'haiku_settings' );
+	?>
+	<input type='text' name='haiku_settings[txt_404]' value='<?php echo $options['txt_404']; ?>'>
 	<?php
 
 }
 
 
-function haiku_text_field_1_render(  ) { 
-
-	$options = get_option( 'haiku_settings' );
-	?>
-	<input type='text' name='haiku_settings[haiku_text_field_1]' value='<?php echo $options['haiku_text_field_1']; ?>'>
-	<?php
+function haiku_settings_section_callback(  ) {
 
 }
 
 
-function haiku_settings_section_callback(  ) { 
-
-}
-
-
-function haiku_options_page(  ) { 
+function haiku_options_page(  ) {
 
 	?>
 	<form action='options.php' method='post'>
 
-		<h2>haiku</h2>
+		<h2>haiku theme options</h2>
 
 		<?php
 		settings_fields( 'pluginPage' );
